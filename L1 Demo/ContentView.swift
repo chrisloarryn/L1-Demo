3be676800bd64ded3bb80f08e358eaa4e51bdf19
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-08-10 23:01:09 -0400

    Initial Commit

---
+++ L1 Demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  L1 Demo
+//
+//  Created by Cristóbal Contreras on 10-08-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ L1 Demo/L1_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoApp.swift
+//  L1 Demo
+//
+//  Created by Cristóbal Contreras on 10-08-23.
+//
+
+import SwiftUI
+
+@main
+struct L1_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
