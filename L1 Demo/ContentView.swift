04790a48b3455a34bc00bc80f96be7cb2f8da609
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-08-10 23:32:25 -0400

    Update ContentView.swift

--- L1 Demo/ContentView.swift
+++ L1 Demo/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        // Color(red: 0, green: 0, blue: 0)
+        // ZStack {
+        // VStack {
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("gophers")
+                    .resizable()
+                    .cornerRadius(15)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.all)
+                
+                Text("gophers")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Text("please be a go language developer uwu")
+                    .font(.headline)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+            }
         }
-        .padding()
     }
 }
 
